6f025d81809107026bebad68ac883b3bd3d8c4db
Anthony Elevathingal <elevathi@usc.edu>
2018-03-01 16:46:04 -0800

    cleanup

--- server/src/main/java/capstone/algorithm/AlgorithmIterator.java
+++ server/src/main/java/capstone/algorithm/AlgorithmIterator.java
@@ -8,7 +8,7 @@ public class AlgorithmIterator {
 	private static String folder_name = "real_data";
 	private static int NUM_RANKED = 5; // number of projects that each student can rank
 	public static Map<Double, Integer> iterations = new HashMap<Double, Integer>();
-	
+		
 	public static void main(String[] args) {
 
 		// create directory for each iteration's output file
