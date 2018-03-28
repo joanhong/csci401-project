7a889ba5d798bbcb5b0e34f55073e39b2f8067b1
Shantanu Gupta <shantang@usc.edu>
2018-03-28 00:14:25 -0700

    added mailDriver, can send email instantly by calling sendEmail

--- server/src/main/java/capstone/PlatformApplication.java
+++ server/src/main/java/capstone/PlatformApplication.java
@@ -3,10 +3,14 @@ package capstone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import mail.mailDriver;
+
 @SpringBootApplication
 public class PlatformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlatformApplication.class, args);
+//		mailDriver mD = new mailDriver("csci401server", "drowssap$$$");
+//		mD.sendEmail("401 Platform Invite!", "Congratulations! The mailDriver totally works!", "shantang@usc.edu");
 	}
 }
